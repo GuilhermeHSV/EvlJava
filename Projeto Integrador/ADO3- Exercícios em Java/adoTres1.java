c04ac7b6a0a59b5b2aa8a5d0122eaa84b1c6a2d4
Guilherme Vasconcellos <guivascon05@gmail.com>
2024-03-17 22:09:55 -0300

    pastas

---
+++ Projeto Integrador/ADO3- Exercícios em Java/adoTres1.java
@@ -0,0 +1,30 @@
+
+package aula3;
+import java.util.Scanner;
+public class teste {
+    public static void main(String[]args){
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        float valorCompra;
+        System.out.println("*** Desconto ***");
+        System.out.print("Qual é o valor da compra?");
+        valorCompra = scanner.nextFloat();
+
+        if (valorCompra >= 300.00){
+           float desconto = (20 * valorCompra) / 100 ;
+           float valorcomdesconto = valorCompra - desconto;
+           System.out.println("O valor descontado foi de: " + valorcomdesconto + "\nE o valor descontado foi de: " + desconto);        
+        } else {
+           float desconto = (15 * valorCompra) / 100;
+           float valorcomdesconto = valorCompra - desconto;
+           System.out.println("O valor descontado foi de " + valorcomdesconto + "\nE o valor descontado foi de: " + desconto);
+        }
+
+
+
+           }
+}
+
+    
+
---
+++ Projeto Integrador/ADO4 - Exercícios em Java/adoQuatro1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class adoQuatro1 {
+    public static void main(String[] args){
+        int diasSemana;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Escolha um número de 1 à 7 que equivale a cada dia da semana " );
+        diasSemana = entrada.nextInt();
+
+        switch (diasSemana) {
+            case 1: 
+            System.out.println("Domingo");
+                break;
+            case 2: 
+            System.out.println("Segunda-Feira");
+                break;
+            case 3:
+            System.out.println("Terça-Feira");
+                break;
+            case 4:
+            System.out.println("Quarta-Feira");
+                break;
+            case 5:
+            System.out.println("Quinta-Feira");
+                break;
+            case 6:
+            System.out.println("Sexta-Feira");
+                break;
+            case 7:
+            System.out.println("Sábado");
+                break;
+            default:
+            System.out.print("O Valor " + diasSemana + " não é válido");
+
+            entrada.close();
+        }
+    }
+}
