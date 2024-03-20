b502455a27b6d3f209812221e53d119312325c4f
Guilherme Vasconcellos <guivascon05@gmail.com>
2024-03-20 16:49:42 -0300

    repeticaoWhile

---
+++ Exercícios em Aula/lacoRepeticaoWhile.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class lacoRepeticaoWhile{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Até qual número você quer que apareça: ");
+        int fim = scanner.nextInt();
+
+        int contagem = 0;
+        while(contagem <= fim){
+            System.out.println(contagem);
+            contagem ++;
+        
+
+
+        }
+        scanner.close();
+    }
+}
