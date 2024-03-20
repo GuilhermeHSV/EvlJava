155ee00f43b3927377a868c73ce11972764cf75e
Guilherme Vasconcellos <guivascon05@gmail.com>
2024-03-20 17:15:08 -0300

    fatorial

---
+++ TADS0/lacoWhileFatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class lacoWhiteFatorial {
+  public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Digite um número para fazer a fatoração dele: ");
+        int num = scanner.nextInt();
+        int volta = num;
+        int fat = 1;
+        while (volta >= 1){
+            fat *= volta;
+
+            
+        System.out.println(fat);
+            volta --;
+         
+        }
+        scanner.close();
+
+    }
+
+}
