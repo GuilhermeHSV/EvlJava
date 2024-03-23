911b3941676677a0b3e1c8d8aa51c22d65ffba1d
Guilherme <guivascon05@gmail.com>
2024-03-22 21:06:23 -0300

    perguntaMate

--- TADS0/lacoWhileFatorial.java
+++ TADS0/lacoWhileFatorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class lacoWhiteFatorial {
+public class lacoWhileFatorial {
   public static void main(String[] args){
         Scanner scanner = new Scanner (System.in);
 
