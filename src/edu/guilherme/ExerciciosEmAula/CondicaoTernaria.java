19f8e49c7ba1bf8bb90def5cce14b2d683510068
Guilherme <guivascon05@gmail.com>
2024-05-02 10:30:38 -0300

    Exercício sobre Condicão Ternária

---
+++ src/edu/guilherme/ExerciciosEmAula/CondicaoTernaria.java
@@ -0,0 +1,12 @@
+package edu.guilherme.ExerciciosEmAula;
+
+public class CondicaoTernaria {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 18;
+
+        String resultado = a == b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+    }
+}
